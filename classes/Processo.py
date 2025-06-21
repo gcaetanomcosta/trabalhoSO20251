feb07c6ee02976c72acdf2d3a75292cb5015525b
lucasgranier <143351703+lucasgranier@users.noreply.github.com>
2025-06-21 17:26:55 -0300

    Update Processo.py

--- classes/Processo.py
+++ classes/Processo.py
@@ -1,2 +1,12 @@
 class Processo:
-    pass
+    def __init__(self, idProcesso, conteudoInicialResidente, subProcessos = None):
+        self.idProcesso = idProcesso
+        self.conteudoInicialResidente = conteudoInicialResidente
+        if subProcessos: self.subProcessos = subProcessos 
+        else: self.subProcessos = []
+
+    def Paginar(self):
+        return list(range(len(self.conteudoInicialResidente)))
+
+    def nPrePaginas(self):
+        return len(self.conteudoInicialResidente)
