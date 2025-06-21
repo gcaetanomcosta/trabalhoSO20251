0da386f8e5e955333acd85b1f11cd1c9d85b3d21
lucasgranier <143351703+lucasgranier@users.noreply.github.com>
2025-06-21 15:58:12 -0300

    Update TLB.py

---
+++ classes/TLB.py
@@ -0,0 +1,24 @@
+from EntradaTLB import EntradaTLB
+
+class TLB:
+  def __init__(self, n_linhas):
+    self.n_linhas = n_linhas
+    self.linhas = []
+
+def acessarTLB(self, idPagina):
+  for i, entrada in enumerate(self.linhas):
+    if entrada.idPagina == idPagina and entrada.validade == 1:
+      self.linhas.append(self.linhas.pop(i))
+      return entrada.endQuadroMP
+  return -1
+
+def atualizarTLB(self, novaEntrada):
+  for i, entrada in enumerate(self.linhas):
+    if entrada.idPagina == novaEntrada.idPagina:
+      self.linhas.pop(i)
+      break
+      
+  if len(self.linhas) >= self.n_linhas:
+    self.linhas.pop(0)
+  self.linhas.append(novaEntrada)
+  
