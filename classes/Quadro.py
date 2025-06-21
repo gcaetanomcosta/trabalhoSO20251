9771ba2f797c46af0fc0ea75c4b2bc094ffbb42e
lucasgranier <143351703+lucasgranier@users.noreply.github.com>
2025-06-21 17:50:19 -0300

    Update Quadro.py

--- classes/Quadro.py
+++ classes/Quadro.py
@@ -1,2 +1,16 @@
+from Pagina import Pagina
+
 class Quadro:
-    pass
+    def __init__(self, end, conteudoQuadro):
+        self.end = end
+        self.conteudoQuadro = conteudoQuadro
+
+    def limpar(self):
+        conteudoQuadro = None
+
+    def alocarPag(self, Pagina):
+        self.conteudoQuadro = Pagina
+
+    def verificarDisponivel(self):
+        if self.conteudoQuadro != None: return 1
+        else: return 0
