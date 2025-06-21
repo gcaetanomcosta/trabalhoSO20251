2b354f0efd2da2a6d0d4213164a92f5ccd30a72b
lucasgranier <143351703+lucasgranier@users.noreply.github.com>
2025-06-21 17:54:03 -0300

    Update Pagina.py

--- classes/Pagina.py
+++ classes/Pagina.py
@@ -1,2 +1,11 @@
 class Pagina:
-    pass
+    def __init__(self, idProcesso, idPagina, conteudoPag):
+        self.idProcesso = idProcesso
+        self.idPagina = idProcesso
+        self.conteudoPag = conteudoPag
+
+    def escrever(self, conteudo):
+        pass
+
+    def let(self):
+        pass
