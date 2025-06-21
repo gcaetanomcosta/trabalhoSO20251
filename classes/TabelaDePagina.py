6f5a763ac16e80af7b43db865fc4b121e8c17414
lucasgranier <143351703+lucasgranier@users.noreply.github.com>
2025-06-21 18:20:43 -0300

    Update TabelaDePagina.py

--- classes/TabelaDePagina.py
+++ classes/TabelaDePagina.py
@@ -1,2 +1,19 @@
 class TabelaDePagina:
-    pass
+    def __init__(self):
+        listaEntradasTP = []
+
+    def adicionarEntrada(self, entrada):
+        self.listaEntradasTP.append(entrada)
+    
+    def ataulizarEnd(self, idPagina, end, bitP, bitM):
+        if 0 <= idPagina < len(self.listaEntradasTP):
+            entrada = self.listaEntradasTP[idPagina]
+            entrada.endQuadroMP = end
+            entrada.bitP = bitP
+            entrada.bitM = bitM
+        else:
+            print(f"Página {idPagina} fora do intervalo.")
+
+    def verificarM(self, idPagina):
+        return self.listaEntradasTP[idPagina].bitM
+        
