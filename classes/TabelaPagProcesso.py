7367464ec4f828b11614062bccd8d780b5077eba
lucasgranier <143351703+lucasgranier@users.noreply.github.com>
2025-06-21 17:01:56 -0300

    Update TabelaPagProcesso.py

--- classes/TabelaPagProcesso.py
+++ classes/TabelaPagProcesso.py
@@ -1,2 +1,23 @@
+from EntradaTPP import EntradaTPP
+from Processo import Processo
+
 class TabelaPagProcesso:
-    pass
+    def __init__(self):
+        self.lista_entradas = []
+
+    def novoProcesso(self, Processo):
+        entrada = EntradaTPP(processo.idProcesso, processo.nPrePaginas())
+        self.lista_entradas.append(entrada)
+        print("Processo {processo.idProcesso} adicionado à tabela")
+
+    def excluirProcesso(self, idProcesso):
+        antes = len(self.lista_entradas)
+        nova_lista = []
+        for e in self.lista_entradas:
+            if e.idProcesso != idProcesso:
+                nova_lista.append(e)
+        self.lista_entradas = nova_lista
+        if antes == len(self.lista_entradas):
+            print("Processo {idProcesso} não encontrado")
+        else:
+            print("Processo {idProcesso} removido da tabela")
